d7dcd75bf137f18932fe64cb52a6f2e42a776510
lal <lalhopeislife77@gmail.com>
2015-10-01 17:33:41 +0545

    newschanges

--- app/Controller/DashboardController.php
+++ app/Controller/DashboardController.php
@@ -19,15 +19,11 @@ class DashboardController extends AppController
        
     }
      function addNews(){
-      // var_dump($_POST);die();
-            $this->loadModel('New');
-            $arr['title']=$_POST['title'];
-            $arr['newscontent']=$_POST['description'];
-           $arr['region_id']=$_POST['select'];
-            $this->New->create();
-            $this->New->save($arr);
-            $this->redirect('news');
+       
+       $arr['is_headline']=$_POST['headline'];    
+       debug($arr);die();   
             
+           
      
     }
     
